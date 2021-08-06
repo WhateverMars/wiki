12c34911fd35472c601e8f5572b36b94c4aefa9d
David Kelly <davekelly1224@gmail.com>
2021-08-06 19:34:08 +0100

    Added comments and removed testing print functions

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -6,49 +6,54 @@ from markdown2 import Markdown
 
 from . import util
 
-
+# markdowner allows markdown to be converted to html back and forth
 markdowner = Markdown()
 
 def index(request):
+    # this page will list all entries
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def entry(request, entry):
+    # this creates a page to display the entry
     data = util.get_entry(entry)
+
+    # if page exists
     if data != None:
         return render(request, "encyclopedia/entry.html", {
             "entry" : markdowner.convert(data),
             "title" : entry
         })
+
+    # if page doesn't exist
     else:
         return render(request, "encyclopedia/entry.html", {
             "entry" : "Sorry requested page not found"
         })
 
 def search(request):
-    
+    # This fn allows the user to search for a page using a string.
     if request.method == "POST" and request.POST.get("q"):
         
         query = request.POST.get("q")
         data = util.get_entry(query)
-        #check if a title matches the search query
+
+        # check if a title matches the search query
         if data != None:
             return render(request, "encyclopedia/entry.html", {
                 "entry" : markdowner.convert(data),
                 "title" : query
             })
 
-        #search fn parsing for substring
+        # search function parsing all entries for substring
+        # This adds all entries that contain the string to a results list
         entries = util.list_entries()
         results = []
         for entry in entries:
             if query.lower() in entry.lower():
-                #print("found")
                 results.append(entry)
 
-        #print(results)
-        #print(len(results))
         
         return render(request,"encyclopedia/search.html", {
             "query" : query,
@@ -63,97 +68,82 @@ def newpage(request):
 
         title = request.POST.get("title")
         
-        #check if inputs are empty
+        # check if inputs are empty
         if not title:
             messages.error(request, "Please provide a title")
             return render(request, "encyclopedia/newpage.html")
 
         content = request.POST.get("content")
-        #print(content)
+
         if not content:
             messages.error(request, "Please provide some content")
             return render(request, "encyclopedia/newpage.html")
 
-        #returns error if title is already in use
+        # returns error if title is already in use
         if util.get_entry(title) != None:
             messages.error(request, "Title already taken")
             return render(request, "encyclopedia/newpage.html")
 
-        #this removes duplicate newlines
+        # this removes duplicate newlines
         content = content.replace("\r","  ")
-        #writes inputs to new file
+
+        # writes inputs to new file
         util.save_entry(title,content)
-        #f = open("entries/"+str(title)+".md", "a")
-        #f.write(content)
-        #f.close()
         
-        #displays new entry
+        
+        # displays new entry
         data = util.get_entry(title)
-        #print(data)
         return render(request, "encyclopedia/entry.html", {
             "entry": markdowner.convert(data),
             "title" : title
         })
 
-    #for error checking
-    #print("method is not post")
     return render(request, "encyclopedia/newpage.html")
 
 
 def edit(request):
 
+    # autofill previous values
     title = request.GET.get("title")
-    #print(title)
     content = util.get_entry(title)
     
     if request.method == "POST":
 
-        
+        # take in posted values
         title = request.POST.get("title")
         
-        #print(title)
-
-        #check if inputs are empty
         content = request.POST.get("content")
         
-        #print(content)
+        # check if inputs are empty
         if not content:
-            #print("content empty")
             messages.error(request, "Please provide some content")
             return render(request, "encyclopedia/newpage.html")
 
-        #this removes duplicate newlines
+        # this removes duplicate newlines
         content = content.replace("\r","  ")
-        #writes inputs to new file
+
+        # writes inputs to new file
         util.save_entry(title,content)
-        #f = open("entries/"+str(title)+".md", "w")
-        #f.write(content)
-        #f.close()
         
-        #displays new entry
         data = util.get_entry(title)
         return render(request, "encyclopedia/entry.html", {
             "entry": markdowner.convert(data),
             "title" : title
         })
 
-    #for error checking
-    #print("method is not post")
+
     return render(request, "encyclopedia/edit.html", {
         "title" : title,
         "content" : content
     })
 
 def random(request):
+    # This allows the user to go to a random wiki entry
 
     import random
-
     
     entries = util.list_entries()
     entry = random.choice(entries)
-    
-
-    #print("Random: "+ entry)
     
     data = util.get_entry(entry)
     return render(request, "encyclopedia/entry.html", {
